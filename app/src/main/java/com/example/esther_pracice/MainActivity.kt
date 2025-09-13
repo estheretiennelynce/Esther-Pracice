24185fca9917dbcc6d75919d26a70bc0dd937f09
estheretiennelynce <esthernyle@gmail.com>
2025-09-13 19:23:52 -0400

    buttonSwitch

--- app/src/main/java/com/example/esther_pracice/MainActivity.kt
+++ app/src/main/java/com/example/esther_pracice/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.esther_pracice
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+        val buttonAddQuestion = findViewById<ImageView>(R.id.add)
+
+        fun launchComposeView() {
+            val i = Intent (this, AddQuestion::class.java)
+            startActivity(i)
+        }
+        buttonAddQuestion.setOnClickListener {
+            launchComposeView()
+        }
         flashcardQuestion.setOnClickListener {
             flashcardQuestion.visibility = View.INVISIBLE
             flashcardAnswer.visibility = View.VISIBLE
